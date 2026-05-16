f81ac46bb3849a92297327bd4c79c54f0eba6f41
kassa_ <pierre.jacobs852@gmail.com>
2026-05-16 21:30:17 +0200

    Fix CI errors

--- include/gqlxy/core/parser/ast/selection.h
+++ include/gqlxy/core/parser/ast/selection.h
@@ -32,22 +32,18 @@ struct InlineFragment {
 
 struct Selection : std::variant<Field, FragmentSpread, InlineFragment> {};
 
+std::string FormatSelection(const Selection&);
+std::string FormatSelectionSet(const SelectionSet&);
+
 }
 
 template <>
 struct std::formatter<gqlxy::parser::SelectionSet> : std::formatter<std::string> {
     auto format(const gqlxy::parser::SelectionSet& ss, std::format_context& ctx) const {
-        std::string result = "{ ";
-        bool first = true;
-        for (const auto& s : ss.selections) {
-            if (!first) result += ' ';
-            result += std::vformat("{}", std::make_format_args(s));
-            first = false;
-        }
-        result += " }";
-        return std::formatter<std::string>::format(result, ctx);
+        return std::formatter<std::string>::format(gqlxy::parser::FormatSelectionSet(ss), ctx);
     }
 };
+static_assert(std::is_copy_constructible_v<std::formatter<gqlxy::parser::SelectionSet, char>>);
 
 template <>
 struct std::formatter<gqlxy::parser::Field> : std::formatter<std::string> {
@@ -60,11 +56,12 @@ struct std::formatter<gqlxy::parser::Field> : std::formatter<std::string> {
             !f.arguments.empty() ? std::format("({})", f.arguments) : "",
             !f.directives.empty() ? std::format(" {}", f.directives) : "",
             gqlxy::utils::and_then(f.selectionSet, [](const auto& s) {
-                return make_optional(std::vformat(" {}", std::make_format_args(s)));
+                return std::make_optional(" " + gqlxy::parser::FormatSelectionSet(s));
             }).value_or("")),
         ctx);
     }
 };
+static_assert(std::is_copy_constructible_v<std::formatter<gqlxy::parser::Field, char>>);
 
 template <>
 struct std::formatter<gqlxy::parser::FragmentSpread> : std::formatter<std::string> {
@@ -74,26 +71,43 @@ struct std::formatter<gqlxy::parser::FragmentSpread> : std::formatter<std::strin
         ctx);
     }
 };
+static_assert(std::is_copy_constructible_v<std::formatter<gqlxy::parser::FragmentSpread, char>>);
 
 template <>
 struct std::formatter<gqlxy::parser::InlineFragment> : std::formatter<std::string> {
     auto format(const gqlxy::parser::InlineFragment& f, std::format_context& ctx) const {
         return std::formatter<std::string>::format(
             std::format("...{}{}", gqlxy::utils::and_then(f.typeCondition, [](const auto& t) {
-                return make_optional(std::format(" on {}", t));
+                return std::make_optional(std::format(" on {}", t));
             }).value_or(""),
             gqlxy::utils::and_then(f.selectionSet, [](const auto& s) {
-                return make_optional(std::vformat(" {}", std::make_format_args(s)));
+                return std::make_optional(" " + gqlxy::parser::FormatSelectionSet(s));
             }).value_or("")
         ), ctx);
     }
 };
+static_assert(std::is_copy_constructible_v<std::formatter<gqlxy::parser::InlineFragment, char>>);
 
 template <>
 struct std::formatter<gqlxy::parser::Selection> : std::formatter<std::string> {
     auto format(const gqlxy::parser::Selection& s, std::format_context& ctx) const {
-        return std::formatter<std::string>::format(std::visit([](const auto& v) {
-            return std::vformat("{}", std::make_format_args(v));
-        }, s), ctx);
+        return std::formatter<std::string>::format(gqlxy::parser::FormatSelection(s), ctx);
     }
 };
+static_assert(std::is_copy_constructible_v<std::formatter<gqlxy::parser::Selection, char>>);
+
+namespace gqlxy::parser {
+
+inline std::string FormatSelection(const Selection& s) {
+    return std::visit([](const auto& v) {
+        return std::vformat("{}", std::make_format_args(v));
+    }, s);
+}
+
+inline std::string FormatSelectionSet(const SelectionSet& ss) {
+    return std::format("{{ {} }}", ss.selections
+        | std::views::transform([](const auto& s) { return FormatSelection(s); })
+        | utils::join_with(" "));
+}
+
+}
